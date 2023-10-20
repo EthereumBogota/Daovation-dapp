20fca47ce380a6e71dfce99e74c8fcd961a66daa
Christian Arevalo <tiancris200@gmail.com>
2023-10-20 18:38:00 -0500

    :lipstick: Updated styles on hero section

--- packages/nextjs/components/landing/Hero.tsx
+++ packages/nextjs/components/landing/Hero.tsx
@@ -2,7 +2,7 @@ export default function Hero(): React.ReactElement {
   return (
     <header className="text-white text-sm py-8 flex flex-col items-center justify-center relative bg-hero-image bg-no-repeat bg-cover bg-bottom bg-fixed">
       <div className="absolute inset-0 bg-shadow"></div>
-      <section className="flex flex-col max-w-[1100px] mx-auto w-full gap-7 md:gap-9 relative z-[5] my-6 md:my-10 items-center justify-center">
+      <section className="flex flex-col max-w-[1100px] h-[70vh] mx-auto w-full gap-7 md:gap-9 relative z-[5] my-6 md:my-10 items-center justify-center">
         <h1 className="md:text-[88px] text-center text-5xl font-bold px-5 md:px-0 leading-tight">
           Transforming Governance with <span className="text-customGold">Active Participation</span>
         </h1>
